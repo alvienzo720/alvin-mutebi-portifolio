3cfe7b89ee0eb441b60542764df56ce222493cfe
alvienzo720 <mutebialvinalvienzo@gmail.com>
2024-11-21 10:16:36 +0300

    Minor Fix

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 
-export async function POST(req: Request) {
+export async function POST(req: any) {
   const body = await req.json()
   const { name, email, message } = body
 
